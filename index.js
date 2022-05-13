1719082080f8b37a4e7173a09e518aedacefed3d
dmstkdrmatkd <dmstkdrmatkd@naver.com>
2022-05-13 21:45:23 +0900

    initial setting

---
+++ index.js
@@ -0,0 +1,64 @@
+//  05/13 초기 설정 및 틀 작성 - 이은상. 아직 따로 설정한 것은 없고 기본 파일 및 서버 열리는 것만 확인
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const dotenv = require('dotenv');
+
+
+const app = express();
+app.set('port', process.env.PORT || 8000);
+
+
+//데이터베이스 연결부. 아직 미설정으로 주석처리
+/*
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('데이터베이스 연결 성공');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+    */
+
+app.use(morgan('dev'));
+//app.use(express.static(path.join(__dirname, '../front-end/public')));   접속시 기본 연결 폴더를 front-end 쪽으로 바꿔놓기 위한 설정 구문. 정확한 위치를 모르니 일단 주석처리
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+}));
+
+
+
+//설정된 라우터 이외의 라우터 이동시 작동구문
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+
+//오류 처리부
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+
+//서버 실행부
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기중');
+});
+
