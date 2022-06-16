54b8e4bb580734e73c07dc4790938b8b87e0a03e
dmstkdrmatkd <dmstkdrmatkd@naver.com>
2022-06-16 14:47:03 +0900

    again

--- routes/main.js
+++ routes/main.js
@@ -21,9 +21,9 @@ const storage = multer({
 });
 var upload = multer({ storage: storage })
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
+//const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const dataProxy = createProxyMiddleware('/data', { target: 'http://localhost:8000/data' });
+//const dataProxy = createProxyMiddleware('/data', { target: 'http://localhost:8000/data' });
 
 
 const router = express.Router();
@@ -46,8 +46,8 @@ router.get('/academicclub', async (req, res, next) => {
     return res.send('hello');
 })
 
-router.get('/data', dataProxy());
-/*
+
+
 router.get('/data', async (req, res, next) => {
     const data = await Info.findAll();
     if (!data) {
@@ -55,7 +55,7 @@ router.get('/data', async (req, res, next) => {
     }
     return res.send(data);
 });
-*/
+
 /*
 models.post.findAll({
     // pagination
